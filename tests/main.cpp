127930b97d36d8410766b6aac040bd42de9bf4a2
mousebyte <ateague063@gmail.com>
2023-03-20 19:30:38 -0400

    Organize tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -27,45 +27,91 @@ static const luaL_Reg funcs[] = {
     {"new", newmyclass},
     {NULL,  NULL      }
 };
+
+int func_for_derived(lua_State *L) {
+    lua_pushstring(L, "Aha! ");
+    lua_insert(L, 1);
+    lua_concat(L, 2);
+    return 1;
+}
 }
 
 TEST_CASE("Basic functions") {
+    using doctest::String;
     lua_State *L = luaL_newstate();
     luaL_openlibs(L);
     luaopen_moonaux(L);
 
-    moonL_dofile(L, "Derived.moon");
+    SUBCASE("Registration") {
+        moonL_dofile(L, "Derived.moon");
 
-    CHECK(lua_type(L, -1) == LUA_TTABLE);
-    CHECK(moonL_isclass(L, -1));
-    CHECK(moonL_registerclass(L, -1));
+        CHECK(lua_type(L, -1) == LUA_TTABLE);
+        CHECK(moonL_isclass(L, -1));
+        CHECK(moonL_registerclass(L, -1));
 
-    lua_pop(L, 1);
+        lua_pop(L, 1);
 
-    CHECK(moonL_getclass(L, "Base") == LUA_TTABLE);
-    CHECK(moonL_isclass(L, -1));
+        CHECK(moonL_getclass(L, "Base") == LUA_TTABLE);
+        CHECK(moonL_isclass(L, -1));
 
-    lua_pop(L, 1);
+        lua_pop(L, 1);
 
-    CHECK(moonL_getclass(L, "Derived") == LUA_TTABLE);
-    CHECK(moonL_isclass(L, -1));
+        CHECK(moonL_getclass(L, "Derived") == LUA_TTABLE);
+        CHECK(moonL_isclass(L, -1));
+        CHECK(lua_getfield(L, -1, "var") == LUA_TSTRING);
+        CHECK(String(lua_tostring(L, -1)) == "Eek!");
 
-    lua_pop(L, 1);
+        lua_pop(L, 2);
+    }
 
     SUBCASE("Construction") {
-        lua_pushstring(L, "Hello, squeak!");
-        moonL_construct(L, 1, "Base");
-        REQUIRE(moonL_isobject(L, -1));
-        REQUIRE(moonL_isinstance(L, -1, "Base"));
-        REQUIRE(lua_getfield(L, -1, "str") == LUA_TSTRING);
-        REQUIRE(doctest::String(lua_tostring(L, -1)) == "Hello, squeak!");
+        moonL_dofile(L, "Derived.moon");
+        REQUIRE(moonL_registerclass(L, -1));
         lua_pop(L, 1);
-        lua_pushnumber(L, 7);
-        moonL_mcall(L, "squeak", 1, 1);
-        REQUIRE(lua_type(L, -1) == LUA_TSTRING);
-        REQUIRE(
-            doctest::String(lua_tostring(L, -1)) == "n is now 7.0, squeak!");
-        lua_pop(L, 3);
+
+        SUBCASE("Base") {
+            lua_pushstring(L, "Hello, squeak!");
+            moonL_construct(L, 1, "Base");
+            REQUIRE(moonL_isobject(L, -1));
+            REQUIRE(moonL_isinstance(L, -1, "Base"));
+            REQUIRE(lua_getfield(L, -1, "str") == LUA_TSTRING);
+            REQUIRE(String(lua_tostring(L, -1)) == "Hello, squeak!");
+            lua_pop(L, 1);
+            lua_pushnumber(L, 7);
+            moonL_mcall(L, "squeak", 1, 1);
+            REQUIRE(lua_type(L, -1) == LUA_TSTRING);
+            REQUIRE(String(lua_tostring(L, -1)) == "n is now 7.0, squeak!");
+            lua_pop(L, 2);
+        }
+
+        SUBCASE("Derived") {
+            lua_pushstring(L, "Whee!");
+            lua_pushcfunction(L, func_for_derived);
+            moonL_construct(L, 2, "Derived");
+            REQUIRE(moonL_isobject(L, -1));
+            REQUIRE(moonL_isinstance(L, -1, "Derived"));
+            REQUIRE(lua_getfield(L, -1, "str") == LUA_TSTRING);
+            REQUIRE(String(lua_tostring(L, -1)) == "Whee!");
+            lua_pop(L, 1);
+            lua_pushnumber(L, 37.1);
+            moonL_mcall(L, "squeak", 1, 1);
+            REQUIRE(lua_type(L, -1) == LUA_TSTRING);
+            REQUIRE(
+                String(lua_tostring(L, -1)) == "Aha! n is now 37.1, squeak!");
+            lua_pop(L, 2);
+        }
+    }
+
+    lua_close(L);
+}
+
+TEST_CASE("C Classes") {
+    using doctest::String;
+    lua_State *L = luaL_newstate();
+    luaL_openlibs(L);
+    luaopen_moonaux(L);
+
+    SUBCASE("Basic C Class") {
         moonL_newclass(L, "myclass", NULL, funcs, NULL);
         REQUIRE(moonL_isclass(L, -1));
         lua_pushnumber(L, 7);
@@ -74,6 +120,4 @@ TEST_CASE("Basic functions") {
         lua_pushnumber(L, 3);
         moonL_mcall(L, "foo", 1, 1);
     }
-
-    lua_close(L);
 }
