82f7376ace330300190f53148a7aa660c5b6fbb4
mousebyte <ateague063@gmail.com>
2023-03-23 17:09:10 -0400

    Add more tests for user data classes

--- tests/userdataclass.cpp
+++ tests/userdataclass.cpp
@@ -1,30 +1,87 @@
 #include "tests.h"
 extern "C" {
 
-#define UCLASS_DATA_FIELDS \
-    FILE *file;            \
+typedef struct BasicClass_Data {
+    int *x;
+} BasicClass_Data;
+
+void BasicClass_alloc(lua_State *L) {
+    BasicClass_Data *o =
+        (BasicClass_Data *)lua_newuserdatauv(L, sizeof(BasicClass_Data), 1);
+    o->x    = (int *)malloc(sizeof(int));
+    *(o->x) = luaL_checknumber(L, 3);
+    lua_remove(L, -2);
+}
+
+void BasicClass_gc(void *p) {
+    BasicClass_Data *o = (BasicClass_Data *)p;
+    if (o->x) {
+        free(o->x);
+        o->x = NULL;
+    }
+}
+
+static moonL_UClass BasicClass {
+    "BasicClass", BasicClass_alloc, BasicClass_gc, 1};
+
+int BasicClass_init(lua_State *L) {
+    moonL_superinit(L);
+    return 0;
+}
+
+int BasicClass_setx(lua_State *L) {
+    BasicClass_Data *o =
+        (BasicClass_Data *)moonL_checkuclass(L, 1, "BasicClass");
+    if (o->x) {
+        *(o->x) = luaL_checknumber(L, 2);
+    }
+    return 0;
+}
+
+int BasicClass_sum(lua_State *L) {
+    BasicClass_Data *o =
+        (BasicClass_Data *)moonL_checkuclass(L, 1, "BasicClass");
+    lua_getfield(L, 1, "n");
+    int x = o->x ? *(o->x) : 0;
+    int n = lua_type(L, -1) == LUA_TNUMBER ? lua_tonumber(L, -1) : 0;
+    lua_pushnumber(L, x + n);
+    return 1;
+}
+
+static luaL_Reg BasicClass_funcs[] = {
+    {"new",  BasicClass_init},
+    {"setx", BasicClass_setx},
+    {"sum",  BasicClass_sum },
+    {NULL,   NULL           }
+};
+
+#define FILECLASS_DATA_FIELDS \
+    FILE *file;               \
     char *name;
 
-typedef struct UClass_Data {
-    UCLASS_DATA_FIELDS
-} UClass_Data;
+typedef struct FileClass_Data {
+    FILECLASS_DATA_FIELDS
+} FileClass_Data;
 
-void UClass_populate_fields(UClass_Data *data, size_t len, const char *str) {
+void FileClass_populate_fields(
+    FileClass_Data *data,
+    size_t          len,
+    const char     *str) {
     data->name = (char *)malloc(len + 1);
     data->file = fopen(str, "r");
     strcpy(data->name, str);
 }
 
-void UClass_alloc(lua_State *L) {
-    UClass_Data *o =
-        (UClass_Data *)lua_newuserdatauv(L, sizeof(UClass_Data), 1);
+void FileClass_alloc(lua_State *L) {
+    FileClass_Data *o =
+        (FileClass_Data *)lua_newuserdatauv(L, sizeof(FileClass_Data), 1);
     size_t      len;
     const char *str = lua_tolstring(L, 3, &len);
-    UClass_populate_fields(o, len, str);
+    FileClass_populate_fields(o, len, str);
 }
 
-void UClass_gc(void *p) {
-    UClass_Data *o = (UClass_Data *)p;
+void FileClass_gc(void *p) {
+    FileClass_Data *o = (FileClass_Data *)p;
     if (o->file) {
         fclose(o->file);
         o->file = NULL;
@@ -35,50 +92,58 @@ void UClass_gc(void *p) {
     }
 }
 
-static moonL_UClass UClass {"UClass", UClass_alloc, UClass_gc, 1};
+static moonL_UClass FileClass {"FileClass", FileClass_alloc, FileClass_gc, 1};
 
-int UClass_init(lua_State *L) {
+int FileClass_init(lua_State *L) {
     lua_pushvalue(L, 2);
     lua_setfield(L, 1, "x");
     return 0;
 }
 
-int UClass_filename(lua_State *L) {
-    UClass_Data *o = (UClass_Data *)moonL_checkuclass(L, 1, "UClass");
+int FileClass_filename(lua_State *L) {
+    FileClass_Data *o = (FileClass_Data *)moonL_checkuclass(L, 1, "FileClass");
     lua_pushstring(L, o->name);
     return 1;
 }
 
-int UClass_readline(lua_State *L) {
+int FileClass_readline(lua_State *L) {
+    FileClass_Data *o = (FileClass_Data *)moonL_checkuclass(L, 1, "FileClass");
+    luaL_Buffer     b;
+    luaL_buffinit(L, &b);
+    int c;
+    while ((c = fgetc(o->file)) != EOF && c != '\n')
+        luaL_addchar(&b, c);
+    luaL_pushresult(&b);
     return 1;
 }
 
-static luaL_Reg UClass_funcs[] = {
-    {"foo",      foo            },
-    {"new",      UClass_init    },
-    {"filename", UClass_filename},
-    {NULL,       NULL           }
+static luaL_Reg FileClass_funcs[] = {
+    {"foo",      foo               },
+    {"new",      FileClass_init    },
+    {"filename", FileClass_filename},
+    {"readline", FileClass_readline},
+    {NULL,       NULL              }
 };
 
-typedef struct DUClass_Data {
-    UCLASS_DATA_FIELDS
+typedef struct DFileClass_Data {
+    FILECLASS_DATA_FIELDS
     size_t arrlen;
     int   *arr;
-} DUClass_Data;
+} DFileClass_Data;
 
-void DUClass_alloc(lua_State *L) {
-    size_t        len;
-    const char   *str = lua_tolstring(L, 3, &len);
-    DUClass_Data *o =
-        (DUClass_Data *)lua_newuserdatauv(L, sizeof(DUClass_Data), 1);
-    UClass_populate_fields((UClass_Data *)o, len, str);
+void DFileClass_alloc(lua_State *L) {
+    size_t           len;
+    const char      *str = lua_tolstring(L, 3, &len);
+    DFileClass_Data *o =
+        (DFileClass_Data *)lua_newuserdatauv(L, sizeof(DFileClass_Data), 1);
+    FileClass_populate_fields((FileClass_Data *)o, len, str);
     size_t num = luaL_checknumber(L, 4);
     o->arr     = (int *)malloc(sizeof(int) * num);
     o->arrlen  = num;
 }
 
-void DUClass_gc(void *p) {
-    DUClass_Data *o = (DUClass_Data *)p;
+void DFileClass_gc(void *p) {
+    DFileClass_Data *o = (DFileClass_Data *)p;
     if (o->arr) {
         free(o->arr);
         o->arr    = NULL;
@@ -86,35 +151,38 @@ void DUClass_gc(void *p) {
     }
 }
 
-static moonL_UClass DUClass {"DUClass", DUClass_alloc, DUClass_gc, 0};
+static moonL_UClass DFileClass {
+    "DFileClass", DFileClass_alloc, DFileClass_gc, 0};
 
-int DUClass_init(lua_State *L) {
+int DFileClass_init(lua_State *L) {
     moonL_superinit(L);
     return 0;
 }
 
-int DUClass_setint(lua_State *L) {
-    DUClass_Data *o     = (DUClass_Data *)moonL_checkuclass(L, 1, "DUClass");
-    size_t        index = luaL_checknumber(L, 2);
+int DFileClass_setint(lua_State *L) {
+    DFileClass_Data *o =
+        (DFileClass_Data *)moonL_checkuclass(L, 1, "DFileClass");
+    size_t index = luaL_checknumber(L, 2);
     if (index < o->arrlen) {
         o->arr[index] = luaL_checknumber(L, 3);
     }
     return 0;
 }
 
-int DUClass_getint(lua_State *L) {
-    DUClass_Data *o     = (DUClass_Data *)moonL_checkuclass(L, 1, "DUClass");
-    size_t        index = luaL_checknumber(L, 2);
+int DFileClass_getint(lua_State *L) {
+    DFileClass_Data *o =
+        (DFileClass_Data *)moonL_checkuclass(L, 1, "DFileClass");
+    size_t index = luaL_checknumber(L, 2);
     if (index < o->arrlen) lua_pushnumber(L, o->arr[index]);
     else lua_pushnil(L);
     return 1;
 }
 
-static luaL_Reg DUClass_funcs[] = {
-    {"new",    DUClass_init  },
-    {"setint", DUClass_setint},
-    {"getint", DUClass_getint},
-    {NULL,     NULL          }
+static luaL_Reg DFileClass_funcs[] = {
+    {"new",    DFileClass_init  },
+    {"setint", DFileClass_setint},
+    {"getint", DFileClass_getint},
+    {NULL,     NULL             }
 };
 }
 
@@ -125,15 +193,15 @@ TEST_CASE("User Data Classes") {
     luaopen_moonaux(L);
 
     SUBCASE("Basic User Data Class") {
-        moonL_newuclass(L, "UClass", NULL, UClass_funcs, &UClass);
+        moonL_newuclass(L, "FileClass", NULL, FileClass_funcs, &FileClass);
         REQUIRE(moonL_isclass(L, -1));
         lua_pop(L, 1);
 
         lua_pushnumber(L, 16);
         lua_pushstring(L, "Derived.moon");
-        moonL_construct(L, 2, "UClass");
+        moonL_construct(L, 2, "FileClass");
         REQUIRE(moonL_isobject(L, -1));
-        REQUIRE(moonL_isinstance(L, -1, "UClass"));
+        REQUIRE(moonL_isinstance(L, -1, "FileClass"));
 
         lua_pushnumber(L, 4);
         moonL_mcall(L, "foo", 1, 1);
@@ -143,52 +211,101 @@ TEST_CASE("User Data Classes") {
         moonL_mcall(L, "filename", 0, 1);
         REQUIRE(lua_type(L, -1) == LUA_TSTRING);
         REQUIRE(String(lua_tostring(L, -1)) == "Derived.moon");
+        lua_pop(L, 1);
+
+        moonL_mcall(L, "readline", 0, 1);
+        REQUIRE(lua_type(L, -1) == LUA_TSTRING);
+        REQUIRE(String(lua_tostring(L, -1)) == "Base = require \"Base\"");
         lua_pop(L, 2);
     }
 
     SUBCASE("Derived User Data Class") {
-        moonL_newuclass(L, "UClass", NULL, UClass_funcs, &UClass);
-        REQUIRE(moonL_isclass(L, -1));
-        lua_pop(L, 1);
+        SUBCASE("From Basic Class") {
+            moonL_dofile(L, "Base.moon");
+            REQUIRE(lua_type(L, -1) == LUA_TTABLE);
+            REQUIRE(moonL_isclass(L, -1));
+            REQUIRE(moonL_registerclass(L, -1));
+            lua_pop(L, 1);
 
-        moonL_newuclass(L, "DUClass", "UClass", DUClass_funcs, &DUClass);
-        REQUIRE(moonL_isclass(L, -1));
-        lua_pop(L, 1);
+            moonL_newuclass(
+                L, "BasicClass", "Base", BasicClass_funcs, &BasicClass);
+            REQUIRE(moonL_isclass(L, -1));
 
-        lua_pushnumber(L, 77);
-        lua_pushstring(L, "Derived.moon");
-        lua_pushnumber(L, 2);
-        moonL_construct(L, 3, "DUClass");
-        REQUIRE(moonL_isobject(L, -1));
-        REQUIRE(moonL_isinstance(L, -1, "DUClass"));
+            REQUIRE(lua_getfield(L, -1, "var") == LUA_TSTRING);
+            REQUIRE(String(lua_tostring(L, -1)) == "Eek!");
+            lua_pop(L, 2);
 
-        lua_pushnumber(L, 2);
-        moonL_mcall(L, "foo", 1, 1);
-        REQUIRE(lua_tonumber(L, -1) == 154);
-        lua_pop(L, 1);
+            lua_pushstring(L, "got any cheese?");
+            lua_pushnumber(L, 666);
+            moonL_construct(L, 2, "BasicClass");
+            REQUIRE(moonL_isobject(L, -1));
+            REQUIRE(moonL_isinstance(L, -1, "BasicClass"));
 
-        moonL_mcall(L, "filename", 0, 1);
-        REQUIRE(lua_type(L, -1) == LUA_TSTRING);
-        REQUIRE(String(lua_tostring(L, -1)) == "Derived.moon");
-        lua_pop(L, 1);
+            moonL_mcall(L, "sum", 0, 1);
+            REQUIRE(lua_tonumber(L, -1) == 666);
+            lua_pop(L, 1);
 
-        lua_pushnumber(L, 0);
-        lua_pushnumber(L, 37);
-        moonL_mcall(L, "setint", 2, 0);
+            lua_pushnumber(L, -444);
+            moonL_mcall(L, "squeak", 1, 1);
+            REQUIRE(lua_type(L, -1) == LUA_TSTRING);
+            REQUIRE(String(lua_tostring(L, -1)) == "n is now -444.0, squeak!");
+            lua_pop(L, 1);
 
-        lua_pushnumber(L, 1);
-        lua_pushnumber(L, 99);
-        moonL_mcall(L, "setint", 2, 0);
+            moonL_mcall(L, "sum", 0, 1);
+            REQUIRE(lua_tonumber(L, -1) == 222);
+            lua_pop(L, 2);
+        }
 
-        lua_pushnumber(L, 0);
-        moonL_mcall(L, "getint", 1, 1);
-        REQUIRE(lua_tonumber(L, -1) == 37);
-        lua_pop(L, 1);
+        SUBCASE("From User Data Class") {
+            moonL_newuclass(L, "FileClass", NULL, FileClass_funcs, &FileClass);
+            REQUIRE(moonL_isclass(L, -1));
+            lua_pop(L, 1);
 
-        lua_pushnumber(L, 1);
-        moonL_mcall(L, "getint", 1, 1);
-        REQUIRE(lua_tonumber(L, -1) == 99);
-        lua_pop(L, 2);
+            moonL_newuclass(
+                L, "DFileClass", "FileClass", DFileClass_funcs, &DFileClass);
+            REQUIRE(moonL_isclass(L, -1));
+            lua_pop(L, 1);
+
+            lua_pushnumber(L, 77);
+            lua_pushstring(L, "Derived.moon");
+            lua_pushnumber(L, 2);
+            moonL_construct(L, 3, "DFileClass");
+            REQUIRE(moonL_isobject(L, -1));
+            REQUIRE(moonL_isinstance(L, -1, "DFileClass"));
+
+            lua_pushnumber(L, 2);
+            moonL_mcall(L, "foo", 1, 1);
+            REQUIRE(lua_tonumber(L, -1) == 154);
+            lua_pop(L, 1);
+
+            moonL_mcall(L, "filename", 0, 1);
+            REQUIRE(lua_type(L, -1) == LUA_TSTRING);
+            REQUIRE(String(lua_tostring(L, -1)) == "Derived.moon");
+            lua_pop(L, 1);
+
+            moonL_mcall(L, "readline", 0, 1);
+            REQUIRE(lua_type(L, -1) == LUA_TSTRING);
+            REQUIRE(String(lua_tostring(L, -1)) == "Base = require \"Base\"");
+            lua_pop(L, 1);
+
+            lua_pushnumber(L, 0);
+            lua_pushnumber(L, 37);
+            moonL_mcall(L, "setint", 2, 0);
+
+            lua_pushnumber(L, 1);
+            lua_pushnumber(L, 99);
+            moonL_mcall(L, "setint", 2, 0);
+
+            lua_pushnumber(L, 0);
+            moonL_mcall(L, "getint", 1, 1);
+            REQUIRE(lua_tonumber(L, -1) == 37);
+            lua_pop(L, 1);
+
+            lua_pushnumber(L, 1);
+            moonL_mcall(L, "getint", 1, 1);
+            REQUIRE(lua_tonumber(L, -1) == 99);
+            lua_pop(L, 2);
+        }
     }
 
     lua_close(L);
