bbac3253ed13ea07f361e4248ddd66e0aaf2f729
mousebyte <ateague063@gmail.com>
2023-04-08 13:36:34 -0400

    Add configuration for Sphinx/Doxygen

---
+++ docs/conf.py
@@ -0,0 +1,56 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+from sphinx.builders.html import StandaloneHTMLBuilder
+import subprocess, os
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Moonlib'
+copyright = '2023, Abigail Teague'
+author = 'Abigail Teague'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+subprocess.call('doxygen Doxyfile.in', shell=True)
+
+extensions = ['breathe']
+
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+highlight_language = 'c'
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'canonical_url': '',
+    'analytics_id': '',  #  Provided by Google in your dashboard
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    
+    'logo_only': False,
+
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
+#html_theme = 'alabaster'
+html_static_path = ['_static']
+
+breathe_projects = {
+	"Moonlib": "_build/xml/"
+}
+breathe_default_project = "Moonlib"
+breathe_default_members = ('members', 'undoc-members')
---
+++ docs/index.rst
@@ -0,0 +1,30 @@
+.. Moonlib documentation master file, created by
+   sphinx-quickstart on Sat Apr  8 12:05:26 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Moonlib's documentation!
+===================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+
+Introduction
+============
+
+Moonlib provides closer integration of Moonscript classes with the Lua C API.
+
+Moonaux
+=======
+
+.. doxygenfile:: moonaux.h
+   :project: Moonlib
+   :sections: briefdescription innerclass public-attrib func define 
